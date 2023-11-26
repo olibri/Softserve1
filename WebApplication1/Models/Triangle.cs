f894cd80f9f460aa5fb38cd4cc113f60894ce3e9
volodya my <linecatdog@gmail.com>
2023-11-26 22:11:26 +0200

    first task

---
+++ WebApplication1/Models/Triangle.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.Models
+{
+    public class Triangle
+    {
+        public double Side_1 { get; set; } 
+        public double Side_2 { get; set; } 
+        public double Side_3 { get; set; }
+        public Triangle()
+        {
+            Side_1 = 5;
+            Side_2 = 12;
+            Side_3 = 8;
+        }
+    }
+}
